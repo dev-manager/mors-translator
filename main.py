4d98bbba9fef61960fe47d31b0dab8ab99faecf0
dev-manager <factorio@naver.com>
2021-04-21 21:39:45 +0900

    code with chinshuu

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from pyperclip import copy
 import sys
 
-ETM = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'}
-MTE = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z'}
+data = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z'}
 
-while True:
-    mode = sys.argv[1]
-    if mode == '-E' or mode == '-e':
-        result = ''
-        sentence = sys.argv[2]
-        for j in sys.argv[2:]:
-            for i in j:
-                i = i.upper()
-                result += ETM.get(i) + ' '
-        print(result)
-        copy(result)
-        print('mors is copied')
-        sentence = ''
-        break
-    elif mode == '-M' or mode == '-m':
-        result = ''
-        sentence = sys.argv[2:]
-        for i in sentence:
-            result += MTE.get(i)
-        print(result.capitalize())
-        sentence = ''
-        break
+
+if sys.argv[1].startswith('.') or sys.argv[1].startswith('-'):
+    sentence = sys.argv[1:]
+else:
+    sentence = list(sys.argv[1].upper())
+result = ''
+for i in sentence:
+    result += data[i]
+    if not sys.argv[1].startswith('.') or sys.argv[1].startswith('-'):
+        result += ' '
+print(result.capitalize())
+copy(result)
+sentence = ''
